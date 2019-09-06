60957920fbc9ba84e810d03372718f120f3a1f1e
HuMart <manya01@protonmail.com>
2019-09-06 12:45:21 -0400

    added : and , to cities. MVP done

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -42,7 +42,7 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c.name, c.lat, c.lon)
+    print(f"{c.name}: Latitude: {c.lat}, Longitude: {c.lon}")
 
 # STRETCH GOAL!
 #
